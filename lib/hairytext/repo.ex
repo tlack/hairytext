d6e23806f86733642fcaa6b785ee1f49f4fb4e03
tlack <lackner@gmail.com>
2020-05-02 03:11:02 +0000

    silence and reformat repo.ex - not as negative as it sounds

--- lib/hairytext/repo.ex
+++ lib/hairytext/repo.ex
@@ -4,26 +4,24 @@ end
 
 defmodule HT.Repo do
   use GenServer
-	import Ecto.Changeset
+  import Ecto.Changeset
 
   def start_link(db_folder) do
-    #IO.inspect(db_folder, label: :HTRepostart_link)
-		state = for schema <- schemas(), into: %{} do
-			fname = Path.join(db_folder, [to_string(schema), ".dets"])
-			{:ok, handle} = :dets.open_file(String.to_atom(fname), file: to_charlist(fname))
-			{schema, handle}
-		end
+    state =
+      for schema <- schemas(), into: %{} do
+        fname = Path.join(db_folder, [to_string(schema), ".dets"])
+        {:ok, handle} = :dets.open_file(String.to_atom(fname), file: to_charlist(fname))
+        {schema, handle}
+      end
 
     GenServer.start_link(__MODULE__, state, name: HTRepo)
   end
 
   def init(state) do
-    #IO.inspect(state, label: :HTRepo_init)
     {:ok, state}
   end
 
   def all(thing) do
-    #IO.inspect(thing, label: :HTRepo_all)
     GenServer.call(HTRepo, {:all, thing})
   end
 
@@ -35,8 +33,7 @@ defmodule HT.Repo do
     GenServer.call(HTRepo, {:get, struct(thing, %{id: id})})
   end
 
-	def insert(%_type{} = thing) when is_struct(thing) do
-		IO.inspect(thing, label: :Repo_insert)
+  def insert(%_type{} = thing) when is_struct(thing) do
     GenServer.call(HTRepo, {:insert, thing})
   end
 
@@ -44,144 +41,123 @@ defmodule HT.Repo do
     GenServer.call(HTRepo, :pids)
   end
 
-	def select(%_type{} = query) when is_struct(query) do
+  def select(%_type{} = query) when is_struct(query) do
     GenServer.call(HTRepo, {:select, query})
   end
 
-	def update(%_type{} = query) when is_struct(query) do
-		# IO.inspect(query, label: Repo_update)
-		IO.inspect(query.data, label: Repo_update_data)
-		IO.inspect(query.changes, label: Repo_update_chgs)
-		GenServer.call(HTRepo, {:update, query})
-	end
+  def update(%_type{} = query) when is_struct(query) do
+    GenServer.call(HTRepo, {:update, query})
+  end
 
   # IMPLEMENTATION
 
   def handle_call({:all, type}, _from, state) when is_atom(type) do
-		handle = Map.get(state, type)
-		data = :dets.match(handle, :'$1')
-		data2 = Enum.map(data, fn x -> make_row(x, type) end)
-		{:reply, data2, state}
+    handle = Map.get(state, type)
+    data = :dets.match(handle, :"$1")
+    data2 = Enum.map(data, fn x -> make_row(x, type) end)
+    {:reply, data2, state}
   end
 
   def handle_call({:all, %type{} = thing}, from, state) do
-		handle_call({:all, type}, from, state)
+    handle_call({:all, type}, from, state)
   end
 
-	def handle_call({:get, %type{} = thing}, _from, state) do
-		handle = Map.get(state, type)
-		id = Map.get(thing, :id)
-		data = get_by_id!(type, handle, id) 
-		{:reply, data, state}
-	end
-
-	def handle_call({:insert, %Ecto.Changeset{valid?: true} = changeset}, _from, state) do
-		with %type{} = value = changeset.data do
-			IO.inspect(type, label: :inserttype)
-			IO.inspect(value, label: :insertdata)
-			IO.inspect(changeset.changes, label: :insertchgs)
-
-			v2 = value 
-				|> unnil(:id, &uuid/0)
-				|> unnil(:inserted_at, &DateTime.utc_now/0)
-				|> unnil(:updated_at, &DateTime.utc_now/0)
-				|> Map.drop([:__meta__, :__schema__, :__struct__])
-				|> Map.merge(changeset.changes)
-
-			IO.inspect(v2, label: :insertv2)
-			id = Map.get(v2, :id)
-			handle = Map.get(state, type)
-			item = {id, v2}
-			case :dets.insert(handle, item) do
-				:ok -> {:reply, {:ok, item}, state}
-				other -> {:reply, {:error, other}, state}
-			end
-		end
-	end
-
-	"""
-  def handle_call({:insert, %type{} = value}, _from, state) do
-		# IO.inspect {type,value}, label: :handle_call_insert
-		v2 = value 
-			|> unnil(:id, &uuid/0)
-			|> unnil(:inserted_at, &DateTime.utc_now/0)
-			|> unnil(:updated_at, &DateTime.utc_now/0)
-			|> Map.drop([:__meta__, :__schema__, :__struct__])
-		IO.inspect(v2, label: :insertv2)
-		id = Map.get(v2, :id)
-		handle = Map.get(state, type)
-		item = {id, v2 |> Map.drop([:id])}
-		:dets.insert(handle, item)
-    {:reply, item, state}
+  def handle_call({:get, %type{} = thing}, _from, state) do
+    handle = Map.get(state, type)
+    id = Map.get(thing, :id)
+    data = get_by_id!(type, handle, id)
+    {:reply, data, state}
   end
-	"""
+
+  def handle_call({:insert, %Ecto.Changeset{valid?: true} = changeset}, _from, state) do
+    with %type{} = value = changeset.data do
+      v2 =
+        value
+        |> unnil(:id, &uuid/0)
+        |> unnil(:inserted_at, &DateTime.utc_now/0)
+        |> unnil(:updated_at, &DateTime.utc_now/0)
+        |> Map.drop([:__meta__, :__schema__, :__struct__])
+        |> Map.merge(changeset.changes)
+
+      id = Map.get(v2, :id)
+      handle = Map.get(state, type)
+      item = {id, v2}
+
+      case :dets.insert(handle, item) do
+        :ok -> {:reply, {:ok, item}, state}
+        other -> {:reply, {:error, other}, state}
+      end
+    end
+  end
 
   def handle_call(:pids, _from, state) do
     {:reply, state, state}
   end
 
-	def handle_call({:select, query}, _from, state) do
-		{:reply, query, state}
-	end
+  def handle_call({:select, query}, _from, state) do
+    {:reply, query, state}
+  end
 
-	def handle_call({:update, %Ecto.Changeset{valid?: true} = changeset}, _from, state) do
-		with %type{} = query = changeset.data do
-      IO.inspect(changeset, label: :chgs)
-			handle = Map.get(state, type)
-			id = Map.get(query, :id)
-			data = get_by_id!(type, handle, id)
-				|> Map.put :updated_at, DateTime.utc_now()
-      data2 = Enum.reduce(changeset.changes, data, 
-        fn {k, v}, acc -> 
-          IO.inspect({k,v}, label: :upd)
-          IO.inspect(acc, label: :upd)
-          Map.put(acc, k, v) 
-        end
-      )
-			data3 = data2 |> Map.drop([:__meta__, :__schema__, :__struct__])
-      IO.inspect({id, data3}, label: :Repo_update)
-			:ok = :dets.insert(handle, {id, data3})
-			{:reply, {:ok, data2}, state}
-		end
-	end
+  def handle_call({:update, %Ecto.Changeset{valid?: true} = changeset}, _from, state) do
+    with %type{} = query = changeset.data do
+      handle = Map.get(state, type)
+      id = Map.get(query, :id)
 
-	defp uuid() do
-		Ecto.UUID.generate()
-	end
+      data =
+        get_by_id!(type, handle, id)
+        |> Map.put(:updated_at, DateTime.utc_now())
 
-	defp unnil(map, key, new_val_fun) do
-		if not Map.has_key?(map, key) or Map.get(map, key) == nil do
-			Map.put(map, key, new_val_fun.())
-		else
-			map
-		end
-	end
+      data2 =
+        Enum.reduce(changeset.changes, data, fn {k, v}, acc ->
+          Map.put(acc, k, v)
+        end)
 
-	defp schemas() do
-		{:ok, modules} = :application.get_key(:hairytext, :modules)
-		modules 
-		|> Enum.filter(&({:__schema__, 1} in &1.__info__(:functions))) 
-	end
+      data3 = data2 |> Map.drop([:__meta__, :__schema__, :__struct__])
+      :ok = :dets.insert(handle, {id, data3})
+      {:reply, {:ok, data2}, state}
+    end
+  end
 
-	defp make_row([ {id, row} ], type) when is_map(row) do
-		make_row({id, row}, type)
-	end
+  defp uuid() do
+    Ecto.UUID.generate()
+  end
 
-	defp make_row({id, row}, type) when is_map(row) do
-		struct(type, row |> Map.put(:id, id))
-	end
+  defp unnil(map, key, new_val_fun) do
+    if not Map.has_key?(map, key) or Map.get(map, key) == nil do
+      Map.put(map, key, new_val_fun.())
+    else
+      map
+    end
+  end
 
-	defp get_by_id!(type, handle, id) do
-		data = case :dets.match(handle, {id, :'$1'}) do
-			[] -> []
-			other ->
-				other
-				|> hd
-				|> hd
-				|> Map.put(:id, id)
-		end
-		struct(type, data)
-	end
+  defp schemas() do
+    {:ok, modules} = :application.get_key(:hairytext, :modules)
 
+    modules
+    |> Enum.filter(&({:__schema__, 1} in &1.__info__(:functions)))
+  end
+
+  defp make_row([{id, row}], type) when is_map(row) do
+    make_row({id, row}, type)
+  end
+
+  defp make_row({id, row}, type) when is_map(row) do
+    struct(type, row |> Map.put(:id, id))
+  end
+
+  defp get_by_id!(type, handle, id) do
+    data =
+      case :dets.match(handle, {id, :"$1"}) do
+        [] ->
+          []
+
+        other ->
+          other
+          |> hd
+          |> hd
+          |> Map.put(:id, id)
+      end
+
+    struct(type, data)
+  end
 end
-
